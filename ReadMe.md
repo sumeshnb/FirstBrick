ca0863c1f3f14690380e4bec2c76703f72d167da
Sumesh Balan <sumesh.nb@gmail.com>
2022-01-28 21:52:37 -0600

    StackString - a fast stack allocated null terminated string

--- ReadMe.md
+++ ReadMe.md
@@ -1,1 +1,4 @@
 Infrastructure code for low latency C++ projects
+
+
+Format code using "clang-format -style=google"
---
+++ Src/StackString.hpp
@@ -0,0 +1,7 @@
+#include <array>
+
+template <std::size_t N>
+class StackString : public std::array<char, N> {
+
+    
+};
